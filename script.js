57daf0143191ff6edf74e53929c5ebef9b8b23ec
Lemonchik120 <godunovao365@gmail.com>
2026-02-09 08:57:01 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,59 +1,86 @@
-let sNumber = Math.floor(Math.random()*100+1);
-let attempts = 0;
-const maxAttempts = 10;
-const messageEl = document.getElementById("message");
-const attemptEl = document.getElementById("attempt");
-const guesInput = document.getElementById("gues");
-function showMessage(text, type = "hint"){
-    messageEl.textContent = text;
-    messageEl.className = type;
+const board = document.getElementById("board");
+const status = document.getElementById("status");
+const resetBtn = document.getElementById("reset");
+let currentPlayer = "X";
+let gameActive = true;
+let gameBoard = ["","","","","","","","",""];
+const winningCombinations = [
+    [0,1,2],[3,4,5],[6,7,8],
+    [0,3,6],[1,4,7],[2,5,8],
+    [0,4,8],[2,4,6],
+];
+for (let i = 0; i < 9; i++){
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.dataset.index = i;
+    cell.addEventListener("click", handleClick);
+    board.appendChild(cell);
+
 
 }
+function handleClick(e){
+    const index = e.target.dataset.index;
+    console.log(gameBoard[index]);
+    if(gameBoard[index] != '' || !gameActive) return;
+    gameBoard[index] = currentPlayer;
+    e.target.textContent = currentPlayer
+    e.target.classList.add(currentPlayer.toLowerCase());
 
 
-function checkGues(){
-    const gues = Number(guesInput.value);
-    if (!gues ||gues < 1 ||gues > 100){
-        showMessage("Некоректна відповідь", "error");
+    if(checkWin()){
+        status.textContent = `Переміг гравець ${currentPlayer}`;
+        status.classList.add("winner");
+        gameActive = false;
+        hightlinerWinningline();
         return;
     }
-    attempts++;
-    attemptEl.textContent = `Спроб: ${attempts}`;
-    if (gues == sNumber){
-        showMessage(`Вітаю! Ти вгадав за ${attempts} спроб`, "success");
-        guesInput.disabled = true;
+    if(gameBoard.every(cell => cell !== "" )){
+        status.textContent = `Нічия`;
+        gameActive = false;
+        return;
     }
-    else if(attempts >= maxAttempts){
-        showMessage(`Спроби закінчились. Було загадано ${sNumber} `, "error");
-        guesInput.disabled = true;
-    }
-    else if(gues > sNumber){
-        showMessage(`Бери менше`, "hint")
-    }
-    else if(gues < sNumber){
-        showMessage(`Бери більше`, "hint")
-    }
-    guesInput.value = "";
-    guesInput.focus();
+    //if (status.textContent == ""){}
+    currentPlayer = currentPlayer === "X"? "O": "X";
+    status.textContent = `Хід гравця ${currentPlayer}`;
+    
 
 }
 
-guesInput.addEventListener("keypress", e => {
-    if(e.key == "Enter"){
-        checkGues();   
-    }
-})
 
+function hightlinerWinningline(){
+    winningCombinations.forEach(comb => {
+        const[a,b,c] = comb;
+        if(gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]){
+            document.querySelector(`[data-index = "${a}"]`).classList.add("winner");
+            document.querySelector(`[data-index = "${b}"]`).classList.add("winner");
+            document.querySelector(`[data-index = "${c}"]`).classList.add("winner");
+        }
 
-function newGame(){
-    sNumber = Math.floor(Math.random()*100+1);
-    attempts = 0;
-    guesInput.disabled = false;
-    guesInput.value = "";
-    attemptEl.textContent = "Спроб 0";
-    showMessage("Нова гра почалась", "hint") 
-    guesInput.focus();
+    });
+}
+
+function checkWin(){
+    return winningCombinations.some(combination => {
+        const[a,b,c] = combination;
+        return gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c];
+
+    });
+
+}
+
+function resetGame(){
+    gameBoard = ["","","","","","","","",""];
+    gameActive = true;
+    currentPlayer = "X";
+    status.textContent = "Хід гравця X";
+    status.classList.remove("winner");
+    document.querySelectorAll('.cell').forEach(cell =>{
+        cell.textContent = "";
+        cell.classList.remove("x","o","winner");
+    });
+    
     
 }
 
 
+resetBtn.addEventListener("click", resetGame);
